192678a027cdcf9c0298dbe4e7dca57cc45a8ab7
Lavanya Singh <lavanyasingh@Lavanya.local>
2019-12-17 16:24:58 -0800

    can parse a diff file and extract line numbers

--- src.py
+++ src.py
@@ -14,16 +14,18 @@ class CommentDater:
     def __init__(self, file):
         self.file= file
 
+    # run git diff in a child process to get diffs
+    # put the diffs in diffs.txt
     def get_diffs(self):
         pid = os.fork()
         
         # set up the child process
         if (pid == 0):
-            args = {"git", "git diff HEAD~1 -U0 %s".format(self.file)}
+            args = ["git", "diff", "HEAD~1", "-U0", self.file]
             
             # set up outfile
-            outfile = open("diffs.txt")
-            os.dup2(outfile, sys.stdout)
+            outfile = open("diffs.txt", "w+")
+            os.dup2(outfile.fileno(), sys.stdout.fileno())
             
             r = os.execvp(args[0], args)
             
@@ -33,13 +35,34 @@ class CommentDater:
                 os._exit(1)
         
         # wait for child to finish
-        r = os.waitpid(pid)
+        r = os.waitpid(pid, 0)
         
-        diffs = open("diffs.txt")
-        print(diffs.read())
+    
+    ## parse a diff line to get line number
+    def parse_line(self, line):
+        line = line[line.find("+")+1:]
+        end = line.find(",") 
+        if end == -1:
+            end = line.find(" @")
+        return line[:end]
+        
+    # return list of changed lines in diff_file
+    def find_lines(self, diff_file):
+        with open(diff_file, "r") as fd:
+            lines = list(iter(fd.readline, ''))
+        lines = list(filter(lambda s: s.find("@@") != -1, lines))
+        for i in range(len(lines)):
+           lines[i] = self.parse_line(lines[i])
+        lines = list(map(lambda s: int(s), lines))
+        return lines
+            
+            
         
 if __name__ == '__main__':
     path = "test.cc"    
+    dater = CommentDater(path)
+    dater.get_diffs()
+    dater.find_lines()
         
             
     
