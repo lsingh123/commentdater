5052749ebb01da723be0aca4c7ea58d4ad0e344d
Lavanya Singh <lavanyasingh@Lavanya.local>
2020-04-06 21:51:06 -0700

    modified test_infile.py

--- commentdater/test_data/test_infile.py
+++ commentdater/test_data/test_infile.py
@@ -8,14 +8,12 @@ Created on Wed Dec 25 12:24:05 2019
 
 # a test file in python - add a line after line 10 for test
 def main():
-    int x = 0
-    
+   
     # initial program code - modify this line for test
-    print("hello world modify this line for test MODIFIED")
+    print("hello world modify this line for test")
     
     '''a multiline comment 
     to see if we can handle that functionality - add a line after 17 to test
     '''
-    int y = 0
     
     return None
